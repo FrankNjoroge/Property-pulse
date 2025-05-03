10a63b09b3c5846e342a2ecbe73312ecc2294684
Frank Njoroge <fnjoro29@gmail.com>
2025-05-03 23:59:41 +0300

    add functionality yo show random properties

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,5 +1,28 @@
+import properties from "@/properties.json";
+import PropertyCard from "./PropertyCard";
+
 const HomeProperties = () => {
-  return <div>HomeProperties</div>;
+  const recentProperties = properties
+    .sort(() => Math.random() - Math.random())
+    .slice(0, 3);
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+          Recent Properties
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {recentProperties.length ? (
+            recentProperties.map((property) => (
+              <PropertyCard property={property} key={property._id} />
+            ))
+          ) : (
+            <p className="text-center">No recent properties found</p>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HomeProperties;
